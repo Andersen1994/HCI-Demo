af07830acf16e210c5007078232e97a67131b459
DESKTOP-JOJMRIH\Andersen <ac941230@gmail.com>
2017-06-07 15:57:45 +0800

    added initial files

---
+++ HCI/Assets/Script/WandController.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO.Ports;
+
+public class WandController : MonoBehaviour {
+    public SerialPort sp = new SerialPort("COM3", 115200);
+    private Valve.VR.EVRButtonId gripButton = Valve.VR.EVRButtonId.k_EButton_Grip;
+    private Valve.VR.EVRButtonId triggerButton = Valve.VR.EVRButtonId.k_EButton_SteamVR_Trigger;
+    private SteamVR_Controller.Device controller { get { return SteamVR_Controller.Input((int)trackedObj.index); } }
+    private SteamVR_TrackedObject trackedObj;
+
+    void Start () {
+        trackedObj = GetComponent<SteamVR_TrackedObject>();
+        sp.Open();
+        sp.ReadTimeout = 12;
+    }
+	
+	
+	void Update () {
+		if(controller == null)
+        {
+            Debug.Log("controller not initialized");
+            return;
+        }
+        if (controller.GetPressDown(gripButton))
+        {
+            Debug.Log("girp button pressed");
+        }
+        if (controller.GetPressUp(gripButton))
+        {
+            Debug.Log("grip button unpressed");
+        }
+	}
+    private void OnTriggerEnter(Collider collider)
+    {
+        switch (collider.tag)
+        {
+            case "fire":
+                HeatUp();
+                break;
+            case "ice":
+                CoolDown();
+                break;
+        }
+
+        Debug.Log("trigger entered");
+    }
+ 
+     private void OnTriggerExit(Collider collider)
+    {
+        switch (collider.tag)
+        {
+            case "fire":
+                CoolDown();
+                break;
+            case "ice":
+                HeatUp();
+                break;
+        }
+        Debug.Log("trigger exit");
+    }
+
+    public void HeatUp()
+    {
+        sp.Write("1");
+    }
+    public void CoolDown()
+    {
+        sp.Write("0");
+    }
+}
---
+++ HCI/Assets/Script/move.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO.Ports;
+
+public class move : MonoBehaviour {
+    public SerialPort sp = new SerialPort("COM3", 115200);
+    bool first = true;
+    int count = 0;
+    int[] serialArray = new int[2];
+    
+    void Start () {
+        sp.Open();
+        sp.ReadTimeout = 20;
+    }
+
+    // Update is called once per frame
+    void Update () {
+        if (sp.IsOpen)
+        {
+            serialArray[count] = sp.ReadByte();
+            count++;
+            if (first)
+            {
+                sp.Write("A");
+                first = false;
+            }
+            else
+            {           
+                if (count > 1)
+                {
+                    MoveObject(serialArray[0], 0);
+                    MoveObject(serialArray[1], 1);
+                    sp.Write("A");
+                    count = 0;
+                 }
+
+            }
+
+        }
+	}
+
+    void MoveObject(int direction,int mode)
+    {
+        if (direction != 0 && mode == 1)
+        {
+            transform.Translate(0.03f, 0, 0);
+        }
+        if (direction != 0 && mode == 0)
+        {
+            transform.Translate(-0.03f, 0, 0);
+        }
+    }
+
+    private void OnTriggerEnter(Collider collider)
+    {
+        switch (collider.tag)
+        {
+            case "fire":
+                Debug.Log("ENTER FIRE");
+                HeatUp();
+                break;
+            case "ice":
+                Debug.Log("ENTER ICE");
+                CoolDown();
+                break;
+        }
+
+        Debug.Log("trigger entered");
+    }
+
+    private void OnTriggerExit(Collider collider)
+    {
+        switch (collider.tag)
+        {
+            case "fire":
+                Debug.Log("LEAVE FIRE");
+                CoolDown();
+                break;
+            case "ice":
+                Debug.Log("LEAVE ICE");
+                HeatUp();
+                break;
+        }
+        Debug.Log("trigger exit");
+    }
+
+    public void HeatUp()
+    {
+        sp.Write("H");
+    }
+    public void CoolDown()
+    {
+        sp.Write("C");
+    }
+}
